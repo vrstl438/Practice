3a65704f1b9e3481965e9582f87ca47e3da3bce0
vrstl438 <ilyafazulzyano73@gmail.com>
2025-11-04 16:13:09 +0300

    Домашнее задание: Коллекции в Java 1/3

---
+++ src/main/java/practice_2/Main.java
@@ -0,0 +1,109 @@
+package practice_2;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        //task-1
+//        List <Integer> num = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1));
+//        num.add(5);
+//        System.out.println(num);
+//
+//        //task-2
+//        List <Integer> nums = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 6, 8, 9));
+//        for (int a : nums){
+//            if (a % 2 == 0){
+//                System.out.println(a);
+//            }
+//        }
+//
+////        task-3
+//        List <String> line = new ArrayList<>();
+//        line.add("q");
+//        line.add("qw");
+//        line.add("qwe");
+//        line.add("qwer");
+//        line.add("qwert");
+//        line.add("qwerty");
+//        String word = "";
+//        for (String len : line){
+//            if (len.length() > word.length()){
+//                word = len;
+//            }
+//        }
+//        System.out.println(word);
+//
+//
+//        //task-4
+//        List <Integer> num = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 10, 12));
+//        int a = 0;
+//        for (int b : num){
+//            a = a + b;
+//        }
+//        System.out.println(a);
+//
+//        //task-5
+//        List <Integer> num = new ArrayList<>(Arrays.asList(1, 2, 4, 20, 5, 10, 12));
+//        int a = 0;
+//        for (int b : num){
+//            if (b > a){
+//                a = b;
+//            }
+//        }
+//        System.out.println(a);
+//
+////        Linked
+////        task-1
+//        List <String> lines = new LinkedList<>(Arrays.asList("q", "qw", "qwe", "qwer", "qwert", "qwerty"));
+//        System.out.println(lines);
+//
+////        task-2
+//        List <String> lines = new LinkedList<>();
+//        lines.add("q");
+//        lines.add("qw");
+//        lines.add("qwe");
+//        System.out.println(lines);
+//
+////        task-3
+//        Deque<String> lines = new LinkedList<>(Arrays.asList("q", "qw", "qwe", "qwer", "qwert", "qwerty"));
+//        System.out.println(lines.getFirst());
+//        System.out.println(lines.getLast());
+//
+//        //task-4
+//        List <Integer> lines = new LinkedList<>(Arrays.asList(1,2,3,5,7,9));
+//                int a = 0;
+//        for (int b : lines){
+//            a = a + b;
+//        }
+//        System.out.println(a);
+//
+////        task-5
+//        List <Integer> lines = new LinkedList<>(Arrays.asList(1,2,3,5,7,9));
+//        ListIterator<Integer> it = lines.listIterator();
+//        while (it.hasNext()){
+//            int b = it.next();
+//            System.out.println(b);
+//        }
+//
+//        System.out.println("-----------");
+//
+//        while (it.hasPrevious()){
+//            int a = it.previous();
+//            System.out.println(a);
+//        }
+//
+////          HashSet
+////          task-1
+//        Set <Integer> nums = new HashSet<>(Arrays.asList(1,2,4,5,6));
+//        System.out.println(nums);
+
+
+
+
+
+
+
+
+
+    }
+}
