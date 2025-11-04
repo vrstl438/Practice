e592115fb4cbed520bc4287f43b9fca3fd6a6725
vrstl438 <ilyafazulzyano73@gmail.com>
2025-11-05 00:04:39 +0300

    Домашнее задание: Коллекции в Java final

---
+++ src/main/java/practice_2/task_1/Contains.java
@@ -0,0 +1,38 @@
+package practice_2.task_1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+
+public class Contains {
+    private static HashSet <Integer> idEmployee = new HashSet<>();
+
+    public static void addEmployees(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите количество добавляемых пользователей:");
+        int count = scanner.nextInt();
+        while (idEmployee.size() < count) {
+            idEmployee.add((int) (Math.random() * 100));
+        }
+        System.out.println("Пользователь добавились!");
+    }
+
+    public static void printAllId(){
+        System.out.println("Перечеь существующих ID");
+        System.out.println(idEmployee);
+    }
+
+    public static void checkId(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ID пользователя");
+        int numberId = scanner.nextInt();
+        boolean check = idEmployee.contains(Integer.valueOf(numberId));
+        if (check){
+            System.out.println("Да, с таким ID пользователь существует");
+        }else {
+            System.out.println("С таким ID пользователя не существует");
+        }
+    }
+
+}
