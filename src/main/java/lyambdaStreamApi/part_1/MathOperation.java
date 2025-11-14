7ba7ce65d3af683317e2d3d55eaa98ad9d1b1513
vrstl438 <ilyafazulzyano73@gmail.com>
2025-11-14 20:24:47 +0300

    Домашнее задание: Функциональные интерфейсы, лямбда-выражения и Stream API

---
+++ src/main/java/lyambdaStreamApi/part_1/MathOperation.java
@@ -0,0 +1,10 @@
+package lyambdaStreamApi.part_1;
+
+@FunctionalInterface
+public interface MathOperation {
+    static void add(int i, int i1) {
+
+    }
+
+    Integer operation(Integer a, Integer b);
+}
---
+++ src/main/java/lyambdaStreamApi/part_2/Main.java
@@ -0,0 +1,47 @@
+package lyambdaStreamApi.part_2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        //task-1
+        List<String> listString = Arrays.asList("qwe", "qwert", "wqerty");
+        List<String> newListString = listString.stream()
+                .filter(a -> a.length() > 5)
+                .toList();
+        System.out.println(newListString);
+
+        //task-2
+        List<Integer> numsList = Arrays.asList(1, 2, 3, 4, 5, 10, 15, 324);
+        List<Integer> newListNums = numsList.stream()
+                .filter(a -> a % 5 == 0)
+                .toList();
+        System.out.println(newListNums);
+
+        //task-3
+        List<String> list = Arrays.asList("qwe", "qwert", "wqerty");
+        List<Integer> newList = list.stream()
+                .map(a -> a.length())
+                .toList();
+        System.out.println(newList);
+
+        //task-4
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 10, 15, 324);
+        List<Integer> newNums = nums.stream()
+                .map(a -> a * a)
+                .toList();
+        System.out.println(newNums);
+
+        //task-5
+        List<Integer> numsDoubleList = Arrays.asList(1, 2, 3, 4, 5, 10, 15, 324, 1, 2, 3, 4, 5, 10, 15, 324);
+        List<Integer> newNumsDoobleList = numsDoubleList.stream()
+                .distinct()
+                .toList();
+        System.out.println(newNumsDoobleList);
+    }
+}
